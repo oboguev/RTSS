31c0a18ada19d35f6a1af4378fdc2e52157ac0b8
Sergey Oboguev <oboguev@yahoo.com>
2024-04-05 01:05:29 -0700

    misc

---
+++ ww2-losses/src/main/java/rtss/ww2losses/p194x/USSR_Expected_Population_In_Early_1946.java
@@ -0,0 +1,6 @@
+package rtss.ww2losses.p194x;
+
+public class USSR_Expected_Population_In_Early_1946
+{
+
+}
--- ww2-losses/src/main/java/rtss/ww2losses/p194x/USSR_Population_In_Early_1946.java
+++
@@ -1,6 +0,0 @@
-package rtss.ww2losses.p194x;
-
-public class USSR_Population_In_Early_1946
-{
-
-}
