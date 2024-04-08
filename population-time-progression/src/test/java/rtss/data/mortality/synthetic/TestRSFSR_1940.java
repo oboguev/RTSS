33b577afa766730d2c2a9e54f19056caadf3afea
Sergey Oboguev <oboguev@yahoo.com>
2024-04-08 10:57:03 -0700

    misc

---
+++ population-time-progression/src/test/java/rtss/data/mortality/synthetic/TestRSFSR_1940.java
@@ -0,0 +1,23 @@
+package rtss.data.mortality.synthetic;
+
+import rtss.data.mortality.CombinedMortalityTable;
+
+public class TestRSFSR_1940
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            CombinedMortalityTable mt = new RSFSR_1940();
+
+            String outputDir = "P:\\@@\\RSFSR_1940"; 
+            String comment = String.format("Таблица построена модулем %s по данным в АДХ-Россия", RSFSR_1940.class.getCanonicalName());
+            
+            mt.saveTable(outputDir, comment);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
