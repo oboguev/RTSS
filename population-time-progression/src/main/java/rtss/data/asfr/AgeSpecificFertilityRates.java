488945a44c209a041b95189917adfd44cc791e07
Sergey Oboguev <oboguev@yahoo.com>
2024-04-10 00:24:03 -0700

    misc

--- population-time-progression/src/main/java/rtss/data/asfr/AgeSpecificFertilityRates.java
+++ population-time-progression/src/main/java/rtss/data/asfr/AgeSpecificFertilityRates.java
@@ -1,5 +1,8 @@
 package rtss.data.asfr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rtss.data.bin.Bin;
 import rtss.data.bin.Bins;
 import rtss.data.population.Population;
@@ -9,14 +12,15 @@ import rtss.data.selectors.Locality;
 
 /**
  * Возрастные коэффициенты женской плодовитости
+ * (годовое количество рождений на 1000 женщин данной возрастной группы)
  */
 public class AgeSpecificFertilityRates
 {
     private Bin[] bins;
     
-    public AgeSpecificFertilityRates(Bin... bins)
+    public AgeSpecificFertilityRates(Bin... bins) throws Exception
     {
-        this.bins = bins; 
+        this.bins = Bins.bins(bins); 
     }
     
     public double forAge(int age)
@@ -30,6 +34,9 @@ public class AgeSpecificFertilityRates
         return 0;
     }
     
+    /*
+     * Годовое количество рождений в данном населении
+     */
     public double births(PopulationByLocality p) throws Exception
     {
         return births(p.forLocality(Locality.TOTAL));
@@ -47,6 +54,9 @@ public class AgeSpecificFertilityRates
         return sum;
     }
     
+    /*
+     * Рождаемость в данном населении (нормированная на оба пола)
+     */
     public double birthRate(PopulationByLocality p) throws Exception
     {
         return birthRate(p.forLocality(Locality.TOTAL));
@@ -55,5 +65,31 @@ public class AgeSpecificFertilityRates
     public double birthRate(Population p) throws Exception
     {
         return 1000 * births(p) / p.sum(Gender.BOTH, 0, Population.MAX_AGE);
+    }
+    
+    /*
+     * Увеличить или уменьшить возрастные коэффициенты рождаемости пропорциональным образом 
+     * (сохраняя их соотношение) так, чтобы итоговая рождаемость в данном населении составила @cbr.
+     * 
+     * Первоначальная структура не изменяется, возвращается новая структура.
+     */
+    public AgeSpecificFertilityRates rescale(PopulationByLocality p, double cbr) throws Exception
+    {
+        return rescale(p.forLocality(Locality.TOTAL), cbr);
+    }
+
+    public AgeSpecificFertilityRates rescale(Population p, double cbr) throws Exception
+    {
+        double scale = cbr / birthRate(p);
+        
+        List<Bin> list = new ArrayList<>();
+        for (Bin bin : bins)
+        {
+            bin = new Bin(bin);
+            bin.avg *= scale;
+            list.add(bin);
+        }
+        
+        return new AgeSpecificFertilityRates(Bins.bins(list));
     }
 }
