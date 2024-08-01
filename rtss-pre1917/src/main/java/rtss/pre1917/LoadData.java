2a06c1ca56d6172d6ca0e4d63d205fac2047438e
Sergey Oboguev <oboguev@yahoo.com>
2024-08-01 10:19:43 -0700

    misc

--- rtss-pre1917/src/main/java/rtss/pre1917/LoadData.java
+++ rtss-pre1917/src/main/java/rtss/pre1917/LoadData.java
@@ -1,5 +1,6 @@
 package rtss.pre1917;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +10,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import rtss.pre1917.data.ColumnHeader;
 import rtss.pre1917.data.RC;
 import rtss.pre1917.data.Territory;
+import rtss.pre1917.data.TerritoryNames;
+import rtss.pre1917.data.TerritoryYear;
 import rtss.util.Util;
 import rtss.util.excel.Excel;
 
@@ -19,17 +22,19 @@ public class LoadData
         try
         {
             new LoadData().loadAllData();
-            Territory.printSeen();
+            TerritoryNames.printSeen();
             Util.out("** Done");
         }
-        catch (Throwable ex) 
+        catch (Throwable ex)
         {
             Util.err("** Exception: ");
             ex.printStackTrace();
         }
     }
     
-    public void loadAllData() throws Exception
+    private Map<String, Territory> territories = new HashMap<>();
+
+    public Map<String, Territory> loadAllData() throws Exception
     {
         loadUGVI("1893-1895", 1893, 1895);
         loadUGVI("1896-1901", 1896, 1901);
@@ -46,57 +51,230 @@ public class LoadData
         loadUGVI("1912");
         loadUGVI("1913");
         loadUGVI("1914");
+        
+        return territories;
     }
-    
+
     private void loadUGVI(String fn) throws Exception
     {
         String fpath = String.format("ugvi/%s.xlsx", fn);
-        XSSFWorkbook wb = Excel.loadWorkbook(fpath);
-        for (int k = 0; k < wb.getNumberOfSheets(); k++)
+
+        try (XSSFWorkbook wb = Excel.loadWorkbook(fpath);)
         {
-            XSSFSheet sheet = wb.getSheetAt(k);
-            List<List<Object>> rc = Excel.readSheet(wb, sheet, fpath);
-            Map<String, Integer> headers = ColumnHeader.getTopHeaders(sheet, rc);
-            
-            if (!headers.containsKey("губ"))
-                throw new Exception("Нет колоники для губернии");
-            int gc = headers.get("губ");
-            for (int nr = 1; nr < rc.size(); nr++)
+            for (int k = 0; k < wb.getNumberOfSheets(); k++)
             {
-                Object o = RC.get(rc, nr, gc);
-                if (o == null)
-                    o = "";
-                String gub = o.toString();
-                gub = Territory.canonic(gub);
+                XSSFSheet sheet = wb.getSheetAt(k);
+                List<List<Object>> rc = Excel.readSheet(wb, sheet, fpath);
+                Map<String, Integer> headers = ColumnHeader.getTopHeaders(sheet, rc);
+                validateHeaders(headers);
+
+                if (!headers.containsKey("губ"))
+                    throw new Exception("Нет колоники для губернии");
+                int gcol = headers.get("губ");
+                scanGubColumn(rc, gcol);
+
+                // scan column "key yyyy" 
+                scanYearColumn(rc, gcol, headers, "р");
+                scanYearColumn(rc, gcol, headers, "с");
+                scanYearColumn(rc, gcol, headers, "п");
+                scanYearColumn(rc, gcol, headers, "чж");
+                scanYearColumn(rc, gcol, headers, "чр");
+                scanYearColumn(rc, gcol, headers, "чу");
             }
-            // ###
         }
-        wb.close();
     }
 
     private void loadUGVI(String fn, int y1, int y2) throws Exception
     {
         String fpath = String.format("ugvi/%s.xlsx", fn);
-        XSSFWorkbook wb = Excel.loadWorkbook(fpath);
-        for (int k = 0; k < wb.getNumberOfSheets(); k++)
+
+        try (XSSFWorkbook wb = Excel.loadWorkbook(fpath);)
         {
-            XSSFSheet sheet = wb.getSheetAt(k);
-            List<List<Object>> rc = Excel.readSheet(wb, sheet, fpath);
-            Map<String, Integer> headers = ColumnHeader.getTopHeaders(sheet, rc);
-            
-            if (!headers.containsKey("губ"))
-                throw new Exception("Нет колоники для губернии");
-            int gc = headers.get("губ");
-            for (int nr = 1; nr < rc.size(); nr++)
+            for (int k = 0; k < wb.getNumberOfSheets(); k++)
             {
-                Object o = RC.get(rc, nr, gc);
-                if (o == null)
-                    o = "";
-                String gub = o.toString();
-                gub = Territory.canonic(gub);
+                XSSFSheet sheet = wb.getSheetAt(k);
+                List<List<Object>> rc = Excel.readSheet(wb, sheet, fpath);
+                Map<String, Integer> headers = ColumnHeader.getTopHeaders(sheet, rc);
+
+                if (!headers.containsKey("губ"))
+                    throw new Exception("Нет колоники для губернии");
+                int gc = headers.get("губ");
+                scanGubColumn(rc, gc);
             }
-            // ###
         }
-        wb.close();
+    }
+
+    private void validateHeaders(Map<String, Integer> headers) throws Exception
+    {
+        for (String h : headers.keySet())
+        {
+            switch (h)
+            {
+            case "губ":
+            case "чр":
+            case "чу":
+            case "р":
+            case "с":
+            case "чж в сл. году":
+                continue;
+            default:
+                break;
+            }
+
+            if (h.startsWith("р ") ||
+                h.startsWith("с ") ||
+                h.startsWith("п ") ||
+                h.startsWith("чж ") ||
+                h.startsWith("чр ") ||
+                h.startsWith("чу "))
+            {
+                continue;
+            }
+
+            throw new Exception("Incorrect header in Excel file");
+        }
+    }
+
+    private void scanGubColumn(List<List<Object>> rc, int gcol) throws Exception
+    {
+        for (int nr = 1; nr < rc.size(); nr++)
+        {
+            Object o = RC.get(rc, nr, gcol);
+            if (o == null)
+                o = "";
+            String gub = o.toString();
+            gub = TerritoryNames.canonic(gub);
+        }
+    }
+
+    private void scanYearColumn(List<List<Object>> rc, int gcol, Map<String, Integer> headers, String what) throws Exception
+    {
+        for (String h : headers.keySet())
+        {
+            if (h.startsWith(what + " ") && h.length() == what.length() + 5)
+            {
+                int year = Integer.parseInt(h.substring(what.length() + 1));
+                scanYearColumn(rc, gcol, what, year, headers.get(h));
+            }
+        }
+    }
+
+    private void scanYearColumn(List<List<Object>> rc, int gcol, String what, int year, int wcol) throws Exception
+    {
+        for (int nr = 1; nr < rc.size(); nr++)
+        {
+            Object o = RC.get(rc, nr, gcol);
+            if (o == null)
+                o = "";
+            String gub = o.toString();
+            gub = TerritoryNames.canonic(gub);
+            if (gub.length() != 0)
+            {
+                o = RC.get(rc, nr, wcol);
+                setValue(gub, year, what, o);
+            }
+        }
+    }
+
+    private Class typeof(String what) throws Exception
+    {
+        switch (what)
+        {
+        case "чж":
+        case "чр":
+        case "чу":
+            return Long.class;
+        case "р":
+        case "с":
+        case "п":
+            return Double.class;
+        default:
+            throw new Exception("Invalid selector");
+        }
+    }
+
+    private void setValue(String gub, int year, String what, Object o) throws Exception
+    {
+        if (o == null)
+            return;
+
+        String so = o.toString();
+        so = Util.despace(so).trim();
+        if (so.length() == 0)
+            return;
+        
+        if (so.equals("-"))
+            return;
+        
+        if (typeof(what) == Double.class)
+        {
+            Double v = null;
+            
+            if (o instanceof Double)
+            {
+                v = (Double) o;
+            }
+            else if (o instanceof Long)
+            {
+                v = ((Long) o).doubleValue();
+            }
+            else if (o instanceof Integer)
+            {
+                v = ((Integer) o).doubleValue();
+            }
+            else if (o instanceof String)
+            {
+                v = Double.parseDouble(so);
+            }
+            else
+            {
+                throw new Exception("Invalid cell data type (for expected Double)");
+            }
+            
+            territoryYear(gub, year).setValue(what, v);
+        }
+        else if (typeof(what) == Long.class)
+        {
+            Long v = null;
+            
+            if (o instanceof Long)
+            {
+                v = ((Long) o).longValue();
+            }
+            else if (o instanceof Integer)
+            {
+                v = ((Integer) o).longValue();
+            }
+            else if (o instanceof String)
+            {
+                v = Long.parseLong(so);
+            }
+            else if (o instanceof Double)
+            {
+                double dv = (Double) o;
+                v = Math.round(dv);
+                if (dv - v != 0)
+                    throw new Exception("Expected cell value: long/integer, actual: double");
+            }
+            else
+            {
+                throw new Exception("Invalid cell data type (for expected Long)");
+            }
+
+            territoryYear(gub, year).setValue(what, v);
+        }
+    }
+
+    private TerritoryYear territoryYear(String gub, int year)
+    {
+        Territory t = territories.get(gub);
+        
+        if (t == null)
+        {
+            t  = new Territory(gub);
+            territories.put(gub, t);
+        }
+
+        return t.territoryYear(year);
     }
 }
---
+++ rtss-pre1917/src/main/java/rtss/pre1917/data/TerritoryYear.java
@@ -0,0 +1,83 @@
+package rtss.pre1917.data;
+
+public class TerritoryYear
+{
+    public TerritoryYear(Territory territory, int year)
+    {
+        this.territory = territory;
+        this.year = year;
+    }
+
+    public final Territory territory;
+    public final int year;
+
+    /*
+     * Рождаемость, смертность и темп естественного роста.
+     * null = no data
+     */
+    public Double cbr;
+    public Double cdr;
+    public Double ngr;
+
+    /*
+     * Население, число рождений и число смертей 
+     * null = no data
+     */
+    public Long population;
+    public Long births;
+    public Long deaths;
+
+    public void setValue(String what, Double v) throws Exception
+    {
+        switch (what)
+        {
+        case "р":
+            if (cbr != null)
+                throw new Exception("Duplicate value");
+            cbr = v;
+            break;
+
+        case "с":
+            if (cdr != null)
+                throw new Exception("Duplicate value");
+            cdr = v;
+            break;
+        
+        case "п":
+            if (ngr != null)
+                throw new Exception("Duplicate value");
+            ngr = v;
+            break;
+        
+        default:
+            throw new Exception("Invalid selector");
+        }
+    }
+
+    public void setValue(String what, Long v) throws Exception
+    {
+        switch (what)
+        {
+        case "чж":
+            if (population != null)
+                throw new Exception("Duplicate value");
+            population = v;
+            break;
+
+        case "чр":
+            if (births != null)
+                throw new Exception("Duplicate value");
+            births = v;
+            break;
+        
+        case "чу":
+            if (deaths != null)
+                throw new Exception("Duplicate value");
+            deaths = v;
+            break;
+        
+        default:
+            throw new Exception("Invalid selector");
+        }
+    }
+}
