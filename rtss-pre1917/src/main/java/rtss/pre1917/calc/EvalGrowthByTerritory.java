648aeacba596723a80bdf9d5550c8eba4786bc7e
I800461 <sergey.oboguev@sap.com>
2024-10-16 20:43:57 -0700

    misc

--- rtss-pre1917/src/main/java/rtss/pre1917/calc/EvalGrowthByTerritory.java
+++ rtss-pre1917/src/main/java/rtss/pre1917/calc/EvalGrowthByTerritory.java
@@ -1,13 +1,14 @@
 package rtss.pre1917.calc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import rtss.pre1917.LoadData;
 import rtss.pre1917.LoadData.LoadOptions;
 import rtss.pre1917.data.Taxon;
 import rtss.pre1917.data.Territory;
-import rtss.pre1917.data.TerritoryDataSet;
 import rtss.pre1917.data.TerritoryYear;
-import rtss.pre1917.data.migration.TotalMigration;
-import rtss.pre1917.eval.EvalGrowthRate;
 import rtss.pre1917.validate.CheckProgressiveAvailable;
 import rtss.util.Util;
 
@@ -30,6 +31,26 @@ public class EvalGrowthByTerritory extends EvalCountryBase
     {
         super("Империя", 1913);
     }
+    
+    private static class TerritoryResult implements Comparable<TerritoryResult >
+    {
+        public final String tname;
+        public final double ngr;
+
+        public TerritoryResult(String tname, double ngr)
+        {
+            this.tname = tname;
+            this.ngr = ngr;
+        }
+
+        @Override
+        public int compareTo(TerritoryResult o)
+        {
+            return (int) Math.signum(o.ngr - this.ngr);
+        }
+    }
+    
+    private List<TerritoryResult> results = new ArrayList<>();
 
     private void eval() throws Exception
     {
@@ -63,6 +84,9 @@ public class EvalGrowthByTerritory extends EvalCountryBase
             if (Taxon.isComposite(tname))
                 continue;
             
+            double ngr_average = 0;
+            int nyears = 0;
+            
             for (int year = 1896; year <= toYear; year++)
             {
                 if (year == 1898 || year == 1905 || year == 1910)
@@ -70,11 +94,28 @@ public class EvalGrowthByTerritory extends EvalCountryBase
                 
                 Territory t = tdsVitalRates.get(tname);
                 TerritoryYear ty = t.territoryYear(year);
+                
                 long pop_vital = ty.progressive_population.total.both;
-
-                double cbr = (PROMILLE * ty.births.total.both) / pop_vital;
-                double cdr = (PROMILLE * ty.deaths.total.both) / pop_vital;
+                double cbr = (PROMILLE * denull(ty.births.total.both)) / pop_vital;
+                double cdr = (PROMILLE * denull(ty.deaths.total.both)) / pop_vital;
+                double ngr = cbr - cdr;
+                ngr_average += ngr;
+                nyears++;
             }
+            
+            results.add(new TerritoryResult(tname, ngr_average / nyears));
         }
+        
+        Collections.sort(results);
+        
+        for (TerritoryResult r : results)
+        {
+            Util.out(String.format("%s %.1f", r.tname, r.ngr));
+        }
+    }
+    
+    private long denull(Long v)
+    {
+        return v != null ? v : 0;
     }
 }
