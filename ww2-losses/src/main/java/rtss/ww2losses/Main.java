875fb10954f738ed0ac86b9045598bcf61467849
Sergey Oboguev <oboguev@yahoo.com>
2024-12-31 05:58:24 -0800

    misc

--- ww2-losses/src/main/java/rtss/ww2losses/Main.java
+++ ww2-losses/src/main/java/rtss/ww2losses/Main.java
@@ -18,7 +18,7 @@ public class Main
         try
         {
             new Main(Area.USSR).main();
-            new Main(Area.RSFSR).main();
+            // new Main(Area.RSFSR).main();
         }
         catch (Exception ex)
         {
