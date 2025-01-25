0f93cc9ab49e8897ebbf8c36c7daee2bf4ba49f0
I800461 <sergey.oboguev@sap.com>
2025-01-24 18:59:55 -0800

    misc

--- ww2-losses/src/main/java/rtss/ww2losses/helpers/PrintHalves.java
+++ ww2-losses/src/main/java/rtss/ww2losses/helpers/PrintHalves.java
@@ -5,6 +5,7 @@ import rtss.data.population.Population;
 import rtss.data.population.PopulationByLocality;
 import rtss.data.selectors.Gender;
 import rtss.data.selectors.Locality;
+import rtss.util.FieldValue;
 import rtss.util.Util;
 import rtss.ww2losses.HalfYearEntry;
 import rtss.ww2losses.util.HalfYearEntries;
@@ -12,6 +13,7 @@ import rtss.ww2losses.util.HalfYearEntries;
 public class PrintHalves
 {
     private static int MAX_AGE = Population.MAX_AGE;
+    private static double PROMILLE = 1000.0;
 
     public static void print(HalfYearEntries<HalfYearEntry> halves) throws Exception
     {
@@ -28,8 +30,14 @@ public class PrintHalves
         Util.out("    ферт.оч   = ожидаемая численность женщин фертильного возраста 15-54 (в среднем за полугодие)");
         Util.out("    ферт.фч   = фактическая численность женщин фертильного возраста 15-54 (в среднем за полугодие)");
         Util.out("    ферт.нис  = накопленное число избыточных смертей женщин фертильного возраста 15-54 (на начало полугодия)");
+        Util.out("    чн.нач    = общая численность населения в начале полугодия");
+        Util.out("    чн.кон    = общая численность населения в конце полугодия");
+        Util.out("    чн.ср     = средняя численность населения за полугодие");
+        Util.out("    чс        = общее фактическое число смертей за полугодие");
+        Util.out("    р         = рождаемость (промилле, для полугодия, но в нормировке на год");
+        Util.out("    с         = смертность (промилле, для полугодия, но в нормировке на год)");
         Util.out("");
-        Util.out("полугодие нал.ос нал.дс мир.ор фр фср.мир фср ферт.оч ферт.фч ферт.нис");
+        Util.out("полугодие нал.ос нал.дс мир.ор фр фср.мир фср ферт.оч ферт.фч ферт.нис чн.нач чн.кон чн.ср чс р с");
         Util.out("");
 
         for (HalfYearEntry he : halves)
@@ -52,23 +60,46 @@ public class PrintHalves
             double fert_loss = 0;
             if (he.accumulated_excess_deaths != null)
                 fert_loss = he.accumulated_excess_deaths.sum(Locality.TOTAL, Gender.FEMALE, 15, 54);
-            
+
             PopulationByLocality p1 = he.p_nonwar_without_births;
             PopulationByLocality p2 = he.next.p_nonwar_without_births;
             PopulationByLocality pavg = p1.avg(p2, ValueConstraint.NONE);
-            double fert_expected = pavg.sum(Locality.TOTAL, Gender.FEMALE, 15, 54);            
+            double fert_expected = pavg.sum(Locality.TOTAL, Gender.FEMALE, 15, 54);
 
-            Util.out(String.format("%s %6s %6s %6s %6s %6s %6s %6s %6s %6s",
+            double total_deaths = d2_minus_d1 + he.expected_nonwar_deaths + he.actual_warborn_deaths;
+            
+            // population
+            double pn1 = he.p_actual_without_births_start.sum(Locality.TOTAL, Gender.BOTH, 0, MAX_AGE);
+            double pn2 = he.p_actual_without_births_end.sum(Locality.TOTAL, Gender.BOTH, 0, MAX_AGE);
+            
+            // ### accrue actual_births - actual_warborn_deaths начиная с he(1) т.е. 1942.вт
+            
+            double pn_avg = (pn1 + pn2) / 2; 
+
+            double cbr = 2 * PROMILLE * he.actual_births / pn_avg;
+            double cdr = 2 * PROMILLE * total_deaths / pn_avg;
+
+            Util.out(String.format("%s %6s %6s %6s %6s %6s %6s %6s %6s %6s %7s %7s %7s %6s %.1f %.1f",
                                    he.toString(),
                                    f2k(he.expected_nonwar_deaths / 1000.0),
                                    f2k(d2_minus_d1 / 1000.0),
                                    f2k(he.expected_nonwar_births / 1000.0),
+                                   // ---
                                    f2k(he.actual_births / 1000.0),
                                    f2k(he.actual_warborn_deaths_baseline / 1000.0),
                                    f2k(he.actual_warborn_deaths / 1000.0),
+                                   // ---
                                    f2k(fert_expected / 1000.0),
                                    f2k(fert_actual / 1000.0),
-                                   f2k(fert_loss / 1000.0)));
+                                   f2k(fert_loss / 1000.0),
+                                   // ---
+                                   f2k(pn1 / 1000.0),
+                                   f2k(pn2 / 1000.0),
+                                   f2k(pn_avg / 1000.0),
+                                   // ---
+                                   f2k(total_deaths / 1000.0),
+                                   cbr,
+                                   cdr));
         }
 
         Util.out("");
@@ -76,6 +107,25 @@ public class PrintHalves
         Util.out("их ожидаемого числа в услових мира обусловлено быстро расширяющейся к основанию демографической пирамидой");
         Util.out("в годах рождения до 1929, т.е. старше 13 лет в 1941 и старше 18 лет в 1946");
         Util.out("");
+    }
+    
+    private double accrue(HalfYearEntry he_from, HalfYearEntry he_to, String field, boolean nullable) throws Exception
+    {
+        double vsum = 0;
+        
+        HalfYearEntry he = he_from;
+        for (;;)
+        {
+            Double v = FieldValue.getDouble(he, field);
+            if (nullable && v == null)
+                v = 0.0;
+            vsum += v;
+
+            if (he == he_to)
+                break;
+        }
+        
+        return vsum;
     }
 
     private static String f2k(double v)
