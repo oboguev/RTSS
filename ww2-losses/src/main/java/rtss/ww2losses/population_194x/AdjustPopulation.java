18b03f72298258e2addd5c13cee64b40fca1ee59
I800461 <sergey.oboguev@sap.com>
2025-01-31 11:45:55 -0800

    misc

--- ww2-losses/src/main/java/rtss/ww2losses/population_194x/AdjustPopulation.java
+++ ww2-losses/src/main/java/rtss/ww2losses/population_194x/AdjustPopulation.java
@@ -1,11 +1,16 @@
 package rtss.ww2losses.population_194x;
 
+import rtss.data.bin.Bin;
 import rtss.data.population.struct.Population;
 import rtss.data.population.struct.PopulationByLocality;
+import rtss.data.selectors.Gender;
 import rtss.data.selectors.Locality;
+import rtss.util.Util;
 
 public class AdjustPopulation
 {
+    protected static final int[] ADH_binning = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 16 };
+
     public Population adjust(Population p) throws Exception
     {
         return p;
@@ -27,5 +32,46 @@ public class AdjustPopulation
             total = adjust(total);
         
         return new PopulationByLocality(total, urban, rural);
+    }
+
+    /*
+     * Перераспределить @amount человек в возраст to_age из возрастов from_ages
+     */
+    protected void redistribute_to(Population p, Gender gender, int to_age, double amount, int... from_ages) throws Exception
+    {
+        p.add(gender, to_age, amount);
+
+        // распределить перенос из "from" пропорционально численности в возрастах from_ages
+        double from_sum = 0;
+        for (int age : from_ages)
+            from_sum += p.get(gender, age);
+
+        for (int age : from_ages)
+        {
+            double v = p.get(gender, age);
+            double share = amount * (v / from_sum);
+            if (share > v)
+                throw new Exception("невозможно перераспределить");
+            p.sub(gender, age, share);
+        }
+    }
+
+    protected void verifyBinning(final int[] binning, Population p1, Population p2) throws Exception
+    {
+        verifyBinning(Gender.MALE, binning, p1, p2);
+        verifyBinning(Gender.FEMALE, binning, p1, p2);
+        verifyBinning(Gender.BOTH, binning, p1, p2);
+    }
+
+    protected void verifyBinning(Gender gender, final int[] binning, Population p1, Population p2) throws Exception
+    {
+        Bin[] b1 = p1.binSumByAge(gender, binning);
+        Bin[] b2 = p2.binSumByAge(gender, binning);
+        Util.assertion(b1.length == b2.length);
+        for (int k = 0; k < b1.length; k++)
+        {
+            if (!Util.same(b1[k].avg, b2[k].avg))
+                throw new Exception("расклад по возрастным корзинам изменился");
+        }
     }
 }
--- ww2-losses/src/main/java/rtss/ww2losses/population_194x/AdjustPopulation1941.java
+++ ww2-losses/src/main/java/rtss/ww2losses/population_194x/AdjustPopulation1941.java
@@ -1,5 +1,8 @@
 package rtss.ww2losses.population_194x;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rtss.data.bin.Bin;
 import rtss.data.population.struct.Population;
 import rtss.data.selectors.Area;
@@ -9,7 +12,6 @@ import rtss.util.plot.PopulationChart;
 
 public class AdjustPopulation1941 extends AdjustPopulation
 {
-    private static final int[] ADH_binning = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 16 };
     private static final int[] ADH_refined_widths = { 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 16 };
     private final Area area;
 
@@ -22,17 +24,19 @@ public class AdjustPopulation1941 extends AdjustPopulation
     public Population adjust(Population p) throws Exception
     {
         Population p0 = p;
+        List<Integer> isolateAgesMale = new ArrayList<>();
+        List<Integer> isolateAgesFemale = new ArrayList<>();
 
         switch (area)
         {
         case USSR:
             p = p.clone();
-            adjust_USSR(p);
+            adjust_USSR(p, isolateAgesMale, isolateAgesFemale);
             break;
 
         case RSFSR:
             p = p.clone();
-            adjust_RSFSR(p);
+            adjust_RSFSR(p, isolateAgesMale, isolateAgesFemale);
             break;
         }
 
@@ -65,7 +69,7 @@ public class AdjustPopulation1941 extends AdjustPopulation
         return p;
     }
 
-    private void adjust_USSR(Population p) throws Exception
+    private void adjust_USSR(Population p, List<Integer> isolateAgesMale, List<Integer> isolateAgesFemale) throws Exception
     {
         // нельзя изменять значения 5-летних групповых корзин,
         // в частности нельзя перераспределять из 0..4 в 5+
@@ -73,11 +77,15 @@ public class AdjustPopulation1941 extends AdjustPopulation
 
         redistribute_to(p, Gender.MALE, 3, 9_103 + 44_000, 2);
         redistribute_to(p, Gender.MALE, 4, 70_125 + 44_000, 2);
+        isolateAgesMale.add(3);
+        isolateAgesMale.add(4);
 
         // redistribute_to(p, Gender.MALE, 5, 1_327, 6);
 
         redistribute_to(p, Gender.FEMALE, 3, 37_471 + 24_000, 2, 1);
         redistribute_to(p, Gender.FEMALE, 4, 89_171 + 24_000, 2, 1);
+        isolateAgesFemale.add(3);
+        isolateAgesFemale.add(4);
 
         // redistribute_to(p, Gender.FEMALE, 5, 13_434, 6);
 
@@ -85,51 +93,8 @@ public class AdjustPopulation1941 extends AdjustPopulation
         p.recalcTotal();
     }
 
-    private void adjust_RSFSR(Population p) throws Exception
+    private void adjust_RSFSR(Population p, List<Integer> isolateAgesMale, List<Integer> isolateAgesFemale) throws Exception
     {
         // ###
-    }
-
-    /* ================================================= */
-
-    /*
-     * Перераспределить @amount человек в возраст to_age из возрастов from_ages
-     */
-    private void redistribute_to(Population p, Gender gender, int to_age, double amount, int... from_ages) throws Exception
-    {
-        p.add(gender, to_age, amount);
-
-        // распределить перенос из "from" пропорционально численности в возрастах from_ages
-        double from_sum = 0;
-        for (int age : from_ages)
-            from_sum += p.get(gender, age);
-
-        for (int age : from_ages)
-        {
-            double v = p.get(gender, age);
-            double share = amount * (v / from_sum);
-            if (share > v)
-                throw new Exception("невозможно перераспределить");
-            p.sub(gender, age, share);
-        }
-    }
-
-    private void verifyBinning(final int[] binning, Population p1, Population p2) throws Exception
-    {
-        verifyBinning(Gender.MALE, binning, p1, p2);
-        verifyBinning(Gender.FEMALE, binning, p1, p2);
-        verifyBinning(Gender.BOTH, binning, p1, p2);
-    }
-
-    private void verifyBinning(Gender gender, final int[] binning, Population p1, Population p2) throws Exception
-    {
-        Bin[] b1 = p1.binSumByAge(gender, binning);
-        Bin[] b2 = p2.binSumByAge(gender, binning);
-        Util.assertion(b1.length == b2.length);
-        for (int k = 0; k < b1.length; k++)
-        {
-            if (!Util.same(b1[k].avg, b2[k].avg))
-                throw new Exception("расклад по возрастным корзинам изменился");
-        }
     }
 }
