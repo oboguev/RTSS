259faa2b4bfe9994b9c9efc4d6bdbcb0e9a711a9
Sergey Oboguev <oboguev@yahoo.com>
2025-02-07 03:54:46 -0800

    misc

--- population-time-progression/src/main/java/rtss/validate_table_193x/ReconstructTable.java
+++ population-time-progression/src/main/java/rtss/validate_table_193x/ReconstructTable.java
@@ -30,6 +30,7 @@ public class ReconstructTable
 
     private void do_main() throws Exception
     {
+        // ### smooth = false?
         final boolean DoSmoothPopulation = true;
 
         final PopulationByLocality p1937_original = un100(PopulationByLocality.census(Area.USSR, 1937)).smooth(DoSmoothPopulation);
