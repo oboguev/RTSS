10a11b4eb64b5937387225996395178bb8c2ab0e
I800461 <sergey.oboguev@sap.com>
2025-12-15 16:02:40 -0800

    misc

--- rtss-losses-199x/src/main/java/rtss/losses199x/BirthsDeficit.java
+++ rtss-losses-199x/src/main/java/rtss/losses199x/BirthsDeficit.java
@@ -1,5 +1,8 @@
 package rtss.losses199x;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import rtss.data.asfr.AgeSpecificFertilityRates;
 import rtss.data.mortality.CombinedMortalityTable;
 import rtss.data.population.projection.ForwardPopulationUR;
@@ -22,17 +25,42 @@ public class BirthsDeficit
     {
         PopulationByLocality p1989 = LoadData.populationCensus1989();
         PopulationContext p = p1989.toPopulationContext();
+        Map<Integer, Double> year2births = new HashMap<>();
 
+        /*
+         * передвижка от 12.1.1989 до 1.1.1990 с шагом 365-11 дней
+         */
         ForwardPopulationUR fw = new ForwardPopulationUR();
         fw.setBirthRateUrban(asfr_urban);
         fw.setBirthRateRural(asfr_rural);
         fw.forward(p, cmt, (365 - 11) / 365.0);
+
+        year2births.put(1989, fw.getObservedBirths() * 365.0 / (365 - 11));
+
+        /*
+         *  передвижка от 1.1.1990 до 1.1.2016 с шагом год
+         */
+        for (int year = 1990; year <= 2015; year++)
+        {
+            fw = new ForwardPopulationUR();
+            fw.setBirthRateUrban(asfr_urban);
+            fw.setBirthRateRural(asfr_rural);
+            fw.forward(p, cmt, 1.0);
+            year2births.put(year, fw.getObservedBirths());
+        }
+
+        Util.out("");
+        Util.out("Ожидаемое число рождений");
+        Util.out("при сохранении возрастных коэффициентов смертности и плодовитости");
+        Util.out("");
+        for (int year = 1989; year <= 2015; year++)
+        {
+            Util.out(String.format("%4d %,9d", year, Math.round(year2births.get(year))));
+        }
+
+        // ####
         Util.noop();
 
-        // ### передвижка от 12.1.1989 до 1.1.1990 с шагом 365-11
-        // ### передвижка от 1.1.1990 до 1.1.2016 с шагом год
-        // ### записывать числа рождений
-        // ### для 1989 умножить на 365.9 / (365-11)  
         // ### сравнить с Map<Integer,Double> mb = LoadData.actualBirths(1989, 2015);
     }
 }
