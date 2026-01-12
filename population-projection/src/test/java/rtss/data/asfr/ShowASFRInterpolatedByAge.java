17313d1707dde87306772e4247ed077e9a7c3d3f
I800461 <sergey.oboguev@sap.com>
2026-01-12 15:06:57 -0800

    misc

--- population-projection/src/test/java/rtss/data/asfr/ShowASFRInterpolatedByAge.java
+++ population-projection/src/test/java/rtss/data/asfr/ShowASFRInterpolatedByAge.java
@@ -1,5 +1,7 @@
 package rtss.data.asfr;
 
+import rtss.data.bin.Bin;
+import rtss.data.bin.Bins;
 import rtss.data.selectors.Area;
 import rtss.util.Util;
 
@@ -14,25 +16,42 @@ public class ShowASFRInterpolatedByAge
             yearly_asfrs = AgeSpecificFertilityRatesByYear.load(Area.RSFSR, "Total");
             // yearly_asfrs = AgeSpecificFertilityRatesByYear.load(Area.USSR);
             // yearly_asfrs = AgeSpecificFertilityRatesByYear.load("age_specific_fertility_rates/survey-1960.xlsx");
-            
-            AgeSpecificFertilityRates asfr = yearly_asfrs.getForYear(1939);
-            AgeSpecificFertilityRates xasfr = InterpolateASFR_ByAge.interpolate(asfr);
-            
-            if (Util.False)
+
+            int[] display_years = { 1927, 1928, 1929, 1936, 1937, 1938, 1939 };
+
+            for (int year : display_years)
             {
-
-                for (int year : yearly_asfrs.years())
-                {
-                    AgeSpecificFertilityRates asfr2 = yearly_asfrs.getForYear(year);
-                    asfr2.display("Возростная плодовитость для " + year + " года");
-                }
+                AgeSpecificFertilityRates asfr = yearly_asfrs.getForYear(year);
+                AgeSpecificFertilityRates xasfr = InterpolateASFR_ByAge.interpolate(asfr);
+                xasfr.display("Женская возрастная плодовитость для " + year + " года");
+                dump(xasfr, "Женская возрастная плодовитость для " + year + " года (рождений в год на 1000 женщин)");
             }
-
-            Util.unused(yearly_asfrs);
         }
         catch (Throwable ex)
         {
             ex.printStackTrace();
         }
+    }
+
+    private static void dump(AgeSpecificFertilityRates asfr, String title) throws Exception
+    {
+        Util.out(title + "");
+        Util.out("");
+        Util.out("возраст плодовитость");
+
+        Bin[] bins = asfr.binsReadonly();
+        Bin firstBin = Bins.firstBin(bins);
+        Bin lastBin = Bins.lastBin(bins);
+
+        for (int age = firstBin.age_x1; age <= lastBin.age_x2; age++)
+        {
+            double fr = asfr.forAge(age);
+            if (fr != 0)
+                Util.out(String.format("%3d %10.4f", age, fr));
+        }
+
+        Util.out("");
+        Util.out("===============================================");
+        Util.out("");
     }
 }
