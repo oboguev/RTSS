8cc28ee854101b19363b406c322be050b5f1887e
I800461 <sergey.oboguev@sap.com>
2026-06-27 20:10:57 -0700

    misc

--- rtss-pre1917/src/main/java/rtss/pre1917/data/TaxonTerritoryFraction.java
+++ rtss-pre1917/src/main/java/rtss/pre1917/data/TaxonTerritoryFraction.java
@@ -2,7 +2,12 @@ package rtss.pre1917.data;
 
 public class TaxonTerritoryFraction
 {
-    TaxonTerritoryFraction[] ttfs = null;
+    private TaxonTerritoryFraction[] ttfs = null;
+    
+    private Integer year1;
+    private Integer year2;
+    private Double fraction1;
+    private Double fraction2;
 
     private double fraction; 
     
@@ -10,12 +15,32 @@ public class TaxonTerritoryFraction
     private TaxonTerritoryFraction()
     {
     }
+    
+    /* =========================================================== */
+
+    public TaxonTerritoryFraction(TaxonTerritoryFraction... ttfs)
+    {
+        if (ttfs.length == 0)
+            throw new IllegalArgumentException("Empty TaxonTerritoryFraction list");
+        this.ttfs = ttfs;
+    }
+
+    public TaxonTerritoryFraction(int year1, double fraction1, int year2, double fraction2)
+    {
+        this.year1 = year1;
+        this.year2 = year2;
+        
+        this.fraction1 = fraction1;
+        this.fraction2 = fraction2;
+    }
 
     public TaxonTerritoryFraction(double fraction)
     {
         this.fraction = fraction;
     }
     
+    /* =========================================================== */
+
     public double fraction(int year)
     {
         if (ttfs != null)
@@ -25,14 +50,20 @@ public class TaxonTerritoryFraction
                 result *= ttf.fraction(year);
             return result;
         }
-        
-        return fraction;
-    }
-
-    public TaxonTerritoryFraction(TaxonTerritoryFraction... ttfs)
-    {
-        if (ttfs.length == 0)
-            throw new IllegalArgumentException("Empty TaxonTerritoryFraction list");
-        this.ttfs = ttfs;
+        else if (year1 != null)
+        {
+            
+            if (year <= year1)
+                return fraction1;
+            else if (year >= year2)
+                return fraction2;
+            else
+                // ### use linear interpolation
+                return 0;
+        }
+        else
+        {
+            return fraction;
+        }
     }
 }
