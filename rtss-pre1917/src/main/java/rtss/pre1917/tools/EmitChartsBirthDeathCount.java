96f5de6e8b9d8a3da9556ffdd51a9e9bad6c5f27
I800461 <sergey.oboguev@sap.com>
2026-09-02 12:36:20 -0700

    misc

--- rtss-pre1917/src/main/java/rtss/pre1917/tools/EmitChartsBirthDeathCount.java
+++ rtss-pre1917/src/main/java/rtss/pre1917/tools/EmitChartsBirthDeathCount.java
@@ -21,12 +21,25 @@ import rtss.util.excel.Excel;
 public class EmitChartsBirthDeathCount
 {
     private static final String OUT_PATH = "c:\\@\\rtss-pre1917\\charts\\birth-death-counts";
-    
+
+    public static enum ChartType
+    {
+        RawSources, Adjusted
+    }
+
     public static void main(String[] args)
     {
         try
         {
-            new EmitChartsBirthDeathCount().do_main();
+            Util.out("=== Emitting RawSources charts");
+            Util.out("");
+            new EmitChartsBirthDeathCount().do_main(ChartType.RawSources);
+
+            Util.out("");
+            Util.out("=== Emitting Adjusted charts");
+            Util.out("");
+            new EmitChartsBirthDeathCount().do_main(ChartType.Adjusted);
+
             Util.out("");
             Util.out("** Done");
         }
@@ -42,44 +55,57 @@ public class EmitChartsBirthDeathCount
     private TerritoryDataSet tdsCSKUnpatched;
     private TerritoryDataSet tdsUGVIUnpatched;
 
-    private void init() throws Exception
+    private void init(ChartType chartType) throws Exception
     {
         tdsCSKPatched = new LoadData().loadEvroChast(LoadOptions.MERGE_CITIES,
                                                      LoadOptions.MERGE_POST1897_REGIONS,
                                                      LoadOptions.DONT_VERIFY,
                                                      LoadOptions.APPLY_PATCHES,
-                                                     LoadOptions.DONT_ADJUST_FEMALE_BIRTHS);
+                                                     (chartType == ChartType.Adjusted) ? LoadOptions.ADJUST_FEMALE_BIRTHS
+                                                                                       : LoadOptions.DONT_ADJUST_FEMALE_BIRTHS,
+                                                     (chartType == ChartType.Adjusted) ? LoadOptions.FILL_MISSING_BD
+                                                                                       : LoadOptions.DONT_FILL_MISSING_BD);
 
         tdsUGVIPatched = new LoadData().loadUGVI(LoadOptions.MERGE_CITIES,
                                                  LoadOptions.MERGE_POST1897_REGIONS,
                                                  LoadOptions.DONT_VERIFY,
                                                  LoadOptions.APPLY_PATCHES,
-                                                 LoadOptions.DONT_ADJUST_FEMALE_BIRTHS,
-                                                 LoadOptions.DONT_FILL_MISSING_BD,
+                                                 (chartType == ChartType.Adjusted) ? LoadOptions.ADJUST_FEMALE_BIRTHS
+                                                                                   : LoadOptions.DONT_ADJUST_FEMALE_BIRTHS,
+                                                 (chartType == ChartType.Adjusted) ? LoadOptions.FILL_MISSING_BD
+                                                                                   : LoadOptions.DONT_FILL_MISSING_BD,
                                                  LoadOptions.DONT_EVAL_SPLIT_ASTRAKHAN,
                                                  LoadOptions.EVAL_MERGE_ASTRAKHAN,
                                                  LoadOptions.DONT_EVAL_PROGRESSIVE);
 
-        tdsCSKUnpatched = new LoadData().loadEvroChast(LoadOptions.MERGE_CITIES,
+        if (chartType == ChartType.RawSources)
+        {
+            tdsCSKUnpatched = new LoadData().loadEvroChast(LoadOptions.MERGE_CITIES,
+                                                           LoadOptions.MERGE_POST1897_REGIONS,
+                                                           LoadOptions.DONT_VERIFY,
+                                                           LoadOptions.DONT_APPLY_PATCHES,
+                                                           (chartType == ChartType.Adjusted) ? LoadOptions.ADJUST_FEMALE_BIRTHS
+                                                                                             : LoadOptions.DONT_ADJUST_FEMALE_BIRTHS,
+                                                           (chartType == ChartType.Adjusted) ? LoadOptions.FILL_MISSING_BD
+                                                                                             : LoadOptions.DONT_FILL_MISSING_BD);
+
+            tdsUGVIUnpatched = new LoadData().loadUGVI(LoadOptions.MERGE_CITIES,
                                                        LoadOptions.MERGE_POST1897_REGIONS,
                                                        LoadOptions.DONT_VERIFY,
                                                        LoadOptions.DONT_APPLY_PATCHES,
-                                                       LoadOptions.DONT_ADJUST_FEMALE_BIRTHS);
-
-        tdsUGVIUnpatched = new LoadData().loadUGVI(LoadOptions.MERGE_CITIES,
-                                                   LoadOptions.MERGE_POST1897_REGIONS,
-                                                   LoadOptions.DONT_VERIFY,
-                                                   LoadOptions.DONT_APPLY_PATCHES,
-                                                   LoadOptions.DONT_ADJUST_FEMALE_BIRTHS,
-                                                   LoadOptions.DONT_FILL_MISSING_BD,
-                                                   LoadOptions.DONT_EVAL_SPLIT_ASTRAKHAN,
-                                                   LoadOptions.EVAL_MERGE_ASTRAKHAN,
-                                                   LoadOptions.DONT_EVAL_PROGRESSIVE);
+                                                       (chartType == ChartType.Adjusted) ? LoadOptions.ADJUST_FEMALE_BIRTHS
+                                                                                         : LoadOptions.DONT_ADJUST_FEMALE_BIRTHS,
+                                                       (chartType == ChartType.Adjusted) ? LoadOptions.FILL_MISSING_BD
+                                                                                         : LoadOptions.DONT_FILL_MISSING_BD,
+                                                       LoadOptions.DONT_EVAL_SPLIT_ASTRAKHAN,
+                                                       LoadOptions.EVAL_MERGE_ASTRAKHAN,
+                                                       LoadOptions.DONT_EVAL_PROGRESSIVE);
+        }
     }
 
-    private void do_main() throws Exception
+    private void do_main(ChartType chartType) throws Exception
     {
-        init();
+        init(chartType);
 
         for (String tname : Util.sort(tdsUGVIPatched.keySet()))
         {
@@ -89,20 +115,35 @@ public class EmitChartsBirthDeathCount
             Util.out(tname);
 
             Territory tUGVIPatched = tdsUGVIPatched.get(tname);
-            Territory tUGVIUnpatched = tdsUGVIUnpatched.get(tname);
             Territory tCSKPatched = tdsCSKPatched.get(tname);
-            Territory tCSKUnpatched = tdsCSKUnpatched.get(tname);
 
-            if (tUGVIPatched == null || tUGVIUnpatched == null)
+            Territory tUGVIUnpatched = tdsUGVIUnpatched == null ? null : tdsUGVIUnpatched.get(tname);
+            Territory tCSKUnpatched = tdsCSKUnpatched == null ? null : tdsCSKUnpatched.get(tname);
+
+            if (tUGVIPatched == null)
                 throw new Exception("Нет территоррии");
 
-            if ((tCSKPatched == null) != (tCSKUnpatched == null))
+            if (chartType == ChartType.RawSources && tUGVIUnpatched == null)
+                throw new Exception("Нет территоррии");
+
+            if (chartType == ChartType.RawSources && (tCSKPatched == null) != (tCSKUnpatched == null))
                 throw new Exception("Нет территоррии");
 
             boolean hasCSK = (tCSKPatched != null);
 
-            XSSFWorkbook wb = Excel
-                    .loadWorkbook(hasCSK ? "excel-templates/birth-death-counts-ugvi-csk.xlsx" : "excel-templates/birth-death-counts-ugvi.xlsx");
+            XSSFWorkbook wb = null;
+            switch (chartType)
+            {
+            case RawSources:
+                wb = Excel.loadWorkbook(hasCSK ? "excel-templates/birth-death-counts-ugvi-csk-raw-sources.xlsx"
+                                               : "excel-templates/birth-death-counts-ugvi-raw-sources.xlsx");
+                break;
+            case Adjusted:
+                wb = Excel.loadWorkbook(hasCSK ? "excel-templates/birth-death-counts-ugvi-csk-adjusted.xlsx"
+                                               : "excel-templates/birth-death-counts-ugvi-adjusted.xlsx");
+                break;
+            }
+
             XSSFSheet sheet = wb.getSheet("data");
             sheet.getRow(0).getCell(5).setCellValue(tname);
 
@@ -153,30 +194,47 @@ public class EmitChartsBirthDeathCount
                     setNumber(sheet, nr, 3, birthsCSKPatched);
                     setNumber(sheet, nr, 4, deathsCSKPatched);
 
-                    setUnpatched(sheet, nr, 6, birthsUGVIPatched, birthsUGVIUnpatched);
-                    setUnpatched(sheet, nr, 7, deathsUGVIPatched, deathsUGVIUnpatched);
-                    setUnpatched(sheet, nr, 8, birthsCSKPatched, birthsCSKUnpatched);
-                    setUnpatched(sheet, nr, 9, deathsCSKPatched, deathsCSKUnpatched);
+                    if (chartType == ChartType.RawSources)
+                    {
+                        setUnpatched(sheet, nr, 6, birthsUGVIPatched, birthsUGVIUnpatched);
+                        setUnpatched(sheet, nr, 7, deathsUGVIPatched, deathsUGVIUnpatched);
+                        setUnpatched(sheet, nr, 8, birthsCSKPatched, birthsCSKUnpatched);
+                        setUnpatched(sheet, nr, 9, deathsCSKPatched, deathsCSKUnpatched);
+                    }
                 }
                 else
                 {
                     setNumber(sheet, nr, 1, birthsUGVIPatched);
                     setNumber(sheet, nr, 2, deathsUGVIPatched);
 
-                    setUnpatched(sheet, nr, 4, birthsUGVIPatched, birthsUGVIUnpatched);
-                    setUnpatched(sheet, nr, 5, deathsUGVIPatched, deathsUGVIUnpatched);
+                    if (chartType == ChartType.RawSources)
+                    {
+                        setUnpatched(sheet, nr, 4, birthsUGVIPatched, birthsUGVIUnpatched);
+                        setUnpatched(sheet, nr, 5, deathsUGVIPatched, deathsUGVIUnpatched);
+                    }
                 }
             }
-            
+
             while (tname.endsWith("."))
                 tname = Util.stripTail(tname, ".");
-            
+
             File dir = new File(OUT_PATH);
+            switch (chartType)
+            {
+            case RawSources:
+                dir = new File(dir, "raw-sources");
+                break;
+            case Adjusted:
+                dir = new File(dir, "adjusted");
+                break;
+            }
             dir.mkdirs();
+
             File fp = new File(dir, tname + ".xlsx");
-            try (OutputStream out = Files.newOutputStream(fp.toPath())) {
+            try (OutputStream out = Files.newOutputStream(fp.toPath()))
+            {
                 wb.write(out);
-            }            
+            }
         }
     }
 
